da907db14d3366b854b39b48b5656a088357a5ef
Ani-Peju <pjanimas099@gmail.com>
2025-08-13 17:55:08 -0400

    Added components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>Tops</a></li>
+                <li><a>Bottoms</a></li>
+                <li><a>Accessories</a></li>
+            </ul>
+
+        </nav>
+    );
+}
+
+export default Navbar;
