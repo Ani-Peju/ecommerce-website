4caadd852ef1fde038bbcfcb34162b88a7a06952
Ani-Peju <pjanimas099@gmail.com>
2025-09-03 14:38:22 -0400

    Added Footer and started using routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import {BrowserRouter,Route,Routes} from "react-router-dom";
+
 function Navbar() {
     return (
         <nav>
---
+++ src/components/TopClothing.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+function TopClothing () {
+
+    return (
+        <div>
+            <Navbar />
+
+            <Footer />
+        </div>
+    );
+
+}
+
+export default TopClothing;
